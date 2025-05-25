d9928da608d89c39d61de2c4093b57b1a8339959
CaptainVarghoss <delano.mower@gmail.com>
2025-05-25 06:57:12 -0700

    Update windows bat files

--- windows-install.bat
+++ windows-install.bat
@@ -1,4 +1,3 @@
 python -m venv venv
 .\venv\Scripts\python.exe -m pip install --upgrade pip
-.\venv\Scripts\activate
-pip install -r requirements.txt
+.\venv\Scripts\python.exe -m pip install -r requirements.txt
