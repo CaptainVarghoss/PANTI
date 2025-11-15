a88be832b9671a6d5b81933e18b1defc6f9849f1
Captain Varghoss <captain.varghoss@gmail.com>
2025-11-15 11:22:35 -0800

    Cleaned up navbar and navbarmenu. Moved duplicated buttons into individual components.

--- frontend/src/components/NavMenu.jsx
+++ frontend/src/components/NavMenu.jsx
@@ -34,7 +34,7 @@ function NavMenuBar({
 
     return (
         <>
-            <div className="nav-menu-bar-sub">
+            <div className="navbar-menu-sub">
                 <div className="layout-buttons">
                     <button className="grid-button" title="Grid Layout">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 -960 960 960">
@@ -87,9 +87,6 @@ function NavMenuBar({
                     onClick={() => setIsSelectMode(!isSelectMode)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 -960 960 960"><path d="M200-200v80q-33 0-56.5-23.5T120-200zm-80-80v-80h80v80zm0-160v-80h80v80zm0-160v-80h80v80zm80-160h-80q0-33 23.5-56.5T200-840zm80 640v-80h80v80zm0-640v-80h80v80zm160 640v-80h80v80zm0-640v-80h80v80zm160 640v-80h80v80zm0-640v-80h80v80zm160 560h80q0 33-23.5 56.5T760-120zm0-80v-80h80v80zm0-160v-80h80v80zm0-160v-80h80v80zm0-160v-80q33 0 56.5 23.5T840-760z"/></svg>
                     <span>Select</span>
-                </button>
-                <button className="trash-button" title="View Trash"> 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 -960 960 960"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120zm400-600H280v520h400zM360-280h80v-360h-80zm160 0h80v-360h-80zM280-720v520z"/></svg>
                 </button>
             </div>
         </>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-// frontend/src/components/Navbar.jsx
-
 import React, {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext'; // Import useAuth
+import NavbarButtons from './NavbarButtons';
+import NavbarMenuButtons from './NavbarMenuButtons';
 import NavSearchBar from './NavSearchBar';
 import NavMenuBar from './NavMenu';
 
@@ -48,16 +48,18 @@ function Navbar({
 
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        {/* Left Nav Menu Toggle Button */}
+      <div className="navbar-main">
+        {/* Left Navbar Buttons */}
         {settings.sidebar_left_enabled && (
-          <button onClick={toggleNavOpen} className="navbar-open-button">
-            {navOpen ? <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor"><path d="m296-224-56-56 240-240 240 240-56 56-184-183-184 183Zm0-240-56-56 240-240 240 240-56 56-184-183-184 183Z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-200 240-440l56-56 184 183 184-183 56 56-240 240Zm0-240L240-680l56-56 184 183 184-183 56 56-240 240Z"/></svg>}
-          </button>
-        )}
-        {/* Left Filter Buttons */}
-        {settings.sidebar_left_enabled && (
-          <div></div>
+          <div className="navbar-buttons side-left">
+            <NavbarButtons
+              navOpen={navOpen}
+              setNavOpen={setNavOpen}
+              toggleNavOpen={toggleNavOpen}
+              handleFilterToggle={handleFilterToggle}
+              filters={filters}
+            />
+          </div>
         )}
 
         {/* Search Bar (visible when authenticated) */}
@@ -75,35 +77,27 @@ function Navbar({
           />
         )}
 
-        {/* Right Filter Buttons */}
+        {/* Right Navbar Buttons */}
         {settings.sidebar_right_enabled && (
-          <>
-            {filters.map(filter => (
-              filter.header_display === true && (
-                <button 
-                  key={filter.id} 
-                  className={`filter-menu-button ${filter.isSelected ? 'active' : ''}`}
-                  onClick={() => handleFilterToggle(filter.id)}
-                  dangerouslySetInnerHTML={{ __html: filter.icon }}
-                  >
-                </button>
-              )
-            ))}
-          </>
-        )}
-        {/* Right Nav Menu Toggle Button */}
-        {settings.sidebar_right_enabled && (
-          <button onClick={toggleNavOpen} className="navbar-open-button">
-            {navOpen ? <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor"><path d="m296-224-56-56 240-240 240 240-56 56-184-183-184 183Zm0-240-56-56 240-240 240 240-56 56-184-183-184 183Z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-200 240-440l56-56 184 183 184-183 56 56-240 240Zm0-240L240-680l56-56 184 183 184-183 56 56-240 240Z"/></svg>}
-          </button>
+          <div className="navbar-buttons side-right">
+            <NavbarButtons
+              navOpen={navOpen}
+              setNavOpen={setNavOpen}
+              toggleNavOpen={toggleNavOpen}
+              handleFilterToggle={handleFilterToggle}
+              filters={filters}
+            />
+          </div>
         )}
       </div>
-      <div className={`nav-menu-bar ${navOpen ? 'open' : 'closed' }`}>
+      <div className={`navbar-menu ${navOpen ? 'open' : 'closed' }`}>
         {/* Left Sidebar Toggle Button */}
         {settings.sidebar_left_enabled && (
-          <button onClick={toggleLeftSidebar} className="navbar-toggle-button navbar-toggle-button--left" aria-label="Open left menu">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z"/></svg>
-          </button>
+          <NavbarMenuButtons
+            side="left"
+            toggleLeftSidebar={toggleLeftSidebar}
+            toggleRightSidebar={toggleRightSidebar}
+          />
         )}
         {isAuthenticated && (
           <NavMenuBar 
@@ -119,10 +113,12 @@ function Navbar({
         )}
         {/* Right Sidebar Toggle Button */}
         {settings.sidebar_right_enabled && (
-          <button onClick={toggleRightSidebar} className="navbar-toggle-button navbar-toggle-button--right" aria-label="Open right menu">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z"/></svg>
-          </button>
-        )}      
+          <NavbarMenuButtons
+            side="right"
+            toggleLeftSidebar={toggleLeftSidebar}
+            toggleRightSidebar={toggleRightSidebar}
+          />
+        )}
       </div>
     </nav>
   );
