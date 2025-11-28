9161339a0f3cdc5b0990f8c55491cccb6b77d4c8
Captain Varghoss <captain.varghoss@gmail.com>
2025-11-27 19:44:52 -0800

    Added tanstack virtual and query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </QueryClientProvider>,
 )
